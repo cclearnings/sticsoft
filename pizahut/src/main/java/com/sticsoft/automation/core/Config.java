18e93122e077252b3eebe83cb35e2f890f22d71d
sticsoft <pizza.testprod@gmail.com>
2020-04-28 06:45:28 +0530

    adding first one

---
+++ pizahut/src/main/java/com/sticsoft/automation/core/Config.java
@@ -0,0 +1,9 @@
+package com.sticsoft.automation.core;
+
+public class Config {
+	
+	public static String defaultAdminUrl = "http://13.251.157.137/piz_admin/";
+	public static String defaultFrontEndUrl = "http://13.251.157.137/piz_ui/";
+	public static String defaultBrowser = "chrome";
+	
+}
---
+++ pizahut/src/main/java/com/sticsoft/automation/tests/admin/Admin.java
@@ -0,0 +1,39 @@
+package com.sticsoft.automation.tests.admin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+
+import com.sticsoft.automation.core.Browser;
+import com.sticsoft.automation.core.Config;
+
+public class Admin {
+
+	WebDriver browser = Browser.start();
+	
+	
+	@Test
+	public void login() {
+		browser.findElement(By.id("Email")).sendKeys("chandra@pizza.com");
+		browser.findElement(By.id("password")).sendKeys("Stic@123$");
+		browser.findElement(By.xpath("//button[text()='Login']")).click();
+	}
+
+	@Test
+	public void loged() {
+		System.out.println("Logged In");
+	}
+
+	@Test(groups = { "Sanity" })
+	public void logisdn() {
+
+		System.out.println("Login done");
+		System.out.println("Smoke Scenario passed");
+	}
+
+	@Test(groups = { "Regression" })
+	public void register() {
+		System.out.println("Registration done");
+	}
+
+}
