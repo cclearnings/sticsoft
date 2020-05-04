4f90e6945f77a779af1bb21a580576f96b083c19
sticsoft <pizza.testprod@gmail.com>
2020-05-04 19:50:57 +0530

    new changes for place order

--- pizahut/src/main/java/com/sticsoft/automation/core/pages/frontend/Checkout.java
+++ pizahut/src/main/java/com/sticsoft/automation/core/pages/frontend/Checkout.java
@@ -1,5 +1,35 @@
 package com.sticsoft.automation.core.pages.frontend;
 
-public class Checkout {
+import java.util.HashMap;
+
+import org.openqa.selenium.support.FindBy;
+
+import com.sticsoft.automation.core.Page;
+
+import ru.yandex.qatools.htmlelements.annotations.Name;
+import ru.yandex.qatools.htmlelements.annotations.Timeout;
+import ru.yandex.qatools.htmlelements.element.Link;
+
+public class Checkout extends Page {
+	
+	@Name("Top Navigation")
+	@Timeout(20)
+	@FindBy(xpath = "//button[contains(text(),'Place Order')]")
+	private Link placeOrder;
+	
+	
+	public HashMap<String, String> getOrderSummary()
+	{
+		return null;
+	}
+	
+	
+	public void selectPlaceOrder()
+	{
+		placeOrder.click();
+	}
+	
+	
+	
 
 }
